723be296c0e6d2a0a62e13e603725ebff4d92de6
ESUM BRANDON <esumbrandon074@gmail.com>
2023-12-19 01:05:27 +0100

    First initialisation of currency project

---
+++ lib/currency_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CurrencyPage extends StatelessWidget {
+  const CurrencyPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text(
+        "Hello there pips",
+        textDirection: TextDirection.ltr,
+      ),
+    ));
+  }
+}
